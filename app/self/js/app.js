f4126555f9acf245ac958cd4b09974bd5fa3119c
Jaume Bonet <jaume.bonet@gmail.com>
2016-03-09 14:18:30 +0100

    setup official url

--- app/self/js/app.js
+++ app/self/js/app.js
@@ -2,7 +2,7 @@
 * @Author: jaumebonet
 * @Date:   2015-05-19 14:25:18
 * @Last Modified by:   Jaume Bonet
-* @Last Modified time: 2016-03-09 14:17:06
+* @Last Modified time: 2016-03-09 14:18:08
 */
 
 (function(){
@@ -21,7 +21,7 @@
             return new Date();
         };
         this.isOfficialURL = function(){
-            return $location.absUrl() == "http://jaumebonet.cat/interactoMIX/";
+            return $location.absUrl() == "http://interactomix.com";
         }
     }]);
 
