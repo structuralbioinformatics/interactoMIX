06f447ee85b73a5c1ecba633af278fec0624778f
Jaume Bonet <jaume.bonet@gmail.com>
2016-03-09 14:17:32 +0100

    url fix

--- app/self/js/app.js
+++ app/self/js/app.js
@@ -2,7 +2,7 @@
 * @Author: jaumebonet
 * @Date:   2015-05-19 14:25:18
 * @Last Modified by:   Jaume Bonet
-* @Last Modified time: 2016-03-09 14:15:58
+* @Last Modified time: 2016-03-09 14:17:06
 */
 
 (function(){
@@ -21,7 +21,7 @@
             return new Date();
         };
         this.isOfficialURL = function(){
-            return $location.absUrl().split('?')[0] == "http://jaumebonet.cat/interactoMIX/";
+            return $location.absUrl() == "http://jaumebonet.cat/interactoMIX/";
         }
     }]);
 
